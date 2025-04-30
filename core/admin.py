bf8e17f53312d2fdeef8b4f3e6c8177949e713b6
suhailvs <suhailvs@gmail.com>
2025-04-30 18:27:24 +0530

    added all models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from core.models import * 
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(UserBalance)
+admin.site.register(Balance)
+admin.site.register(Transaction)
+admin.site.register(TransactionRecord)
+admin.site.register(Currency)
+admin.site.register(Resolution)
+admin.site.register(UserResolution)
+admin.site.register(ExchangeRate)
